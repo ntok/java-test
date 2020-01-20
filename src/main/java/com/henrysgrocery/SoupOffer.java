6a8767adede7e3749a87e7bf88caf4b729a75a9a
ntok <necati.tok@yahoo.com>
2020-01-20 03:29:15 +0000

    initial unit test for Soup offer implemented

--- src/main/java/com/henrysgrocery/SoupOffer.java
+++ src/main/java/com/henrysgrocery/SoupOffer.java
@@ -3,8 +3,16 @@ package com.henrysgrocery;
 import java.math.BigDecimal;
 import java.util.List;
 
+import static com.henrysgrocery.Item.BREAD;
+import static java.util.stream.Collectors.counting;
+
 public class SoupOffer {
     public BigDecimal calculateDiscount(List<String> shoppingList) {
-        return null;
+        int soupCount = shoppingList.stream().filter(item -> item.equals("SOUP")).collect(counting()).intValue();
+        BigDecimal discountPerQuantity=BREAD.getPrice().divide(BigDecimal.valueOf(2));
+        BigDecimal discountQuantity = BigDecimal.valueOf(soupCount / 2);
+
+        return discountPerQuantity.multiply(discountQuantity);
     }
+
 }
