dcddbbf77a56a1aacfac53eedc40797e7c9a7598
ntok <necati.tok@yahoo.com>
2020-01-21 02:38:29 +0000

    second unit test added for soup offer -- failing

--- src/test/java/com/henrysgrocery/SoupOfferTest.java
+++ src/test/java/com/henrysgrocery/SoupOfferTest.java
@@ -1,8 +1,11 @@
 package com.henrysgrocery;
 
+import org.junit.Before;
 import org.junit.Test;
 
+import java.io.ByteArrayOutputStream;
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,10 +16,24 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class SoupOfferTest {
 
+    private SoupOffer soupOffer;
+
+    @Before
+    public void setUp() {
+        soupOffer = new SoupOffer();
+    }
+
     @Test
     public void whenBasketHasEnoughSoupsAndEnoughBreadForDiscount_thenCalculateBreadDiscount() {
-        BigDecimal discount = new SoupOffer().calculateDiscount(asList(SOUP, SOUP, BREAD));
+        BigDecimal discount = soupOffer.calculateDiscount(asList(SOUP, SOUP, BREAD));
 
         assertThat(discount).isEqualByComparingTo(BigDecimal.valueOf(0.40));
+    }
+
+    @Test
+    public void whenBasketHasManySoupsAndLessBreadCostThenDiscount_thenCalculateBreadDiscount() {
+        BigDecimal discount = soupOffer.calculateDiscount(asList(SOUP, SOUP, SOUP, SOUP, SOUP, SOUP, BREAD));
+
+        assertThat(discount).isEqualByComparingTo(BigDecimal.valueOf(0.80));
     }
 }
