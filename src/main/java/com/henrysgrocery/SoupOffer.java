e6b3f36d1fba2dff46cd898805734940a440785d
ntok <necati.tok@yahoo.com>
2020-01-21 02:42:39 +0000

    second test implemented for soup offer

--- src/main/java/com/henrysgrocery/SoupOffer.java
+++ src/main/java/com/henrysgrocery/SoupOffer.java
@@ -6,12 +6,18 @@ import java.util.List;
 import static com.henrysgrocery.Item.BREAD;
 
 public class SoupOffer {
-    public BigDecimal calculateDiscount(List<Item> shoppingList) {
-        long soupCount = (shoppingList.stream().filter(item -> item.equals(Item.SOUP)).count());
-        BigDecimal discountPerQuantity = BREAD.getPrice().divide(BigDecimal.valueOf(2));
-        BigDecimal discountQuantity = BigDecimal.valueOf(soupCount / 2);
+    private static final BigDecimal DISCOUNT_PER_QUANTITY = BREAD.getPrice().divide(BigDecimal.valueOf(2));
 
-        return discountPerQuantity.multiply(discountQuantity);
+    public BigDecimal calculateDiscount(List<Item> shoppingList) {
+
+        long soupCount = (shoppingList.stream().filter(item -> item.equals(Item.SOUP)).count());
+        BigDecimal discountQuantity = BigDecimal.valueOf(soupCount / 2);
+        BigDecimal totalDiscountFromOffer = DISCOUNT_PER_QUANTITY.multiply(discountQuantity);
+
+        long breadCount = (shoppingList.stream().filter(item -> item.equals(Item.BREAD)).count());
+        BigDecimal breadCost = BREAD.getPrice().multiply(BigDecimal.valueOf(breadCount));
+
+        return breadCost.min(totalDiscountFromOffer);
     }
 
 }
